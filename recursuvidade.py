91151a9237b74abe010f15caaab2f64caee9f4bc
valenca1204 <pedrorsvalenca@gmail.com>
2025-03-12 22:45:46 -0300

    Update recursuvidade.py

--- recursuvidade.py
+++ recursuvidade.py
@@ -1,13 +1,37 @@
-def fatorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * fatorial(n-1)
-n = int(input("digite um úmero inteiro positivo para descobrir o seu fatorial:"))
-print(fatorial(n))
+# def fatorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         return n * fatorial(n-1)
+# n = int(input("digite um úmero inteiro positivo para descobrir o seu fatorial:"))
+# print(fatorial(n))
 
 
 
 
-def soma_lista(lista):
+# def soma_lista(list):
+#     soma = 0
+#     list = [1,2,3,4,5]
+#     for i in list:
+#         soma += i
+        
+#     return soma
+
+# resultado = soma_lista([1, 2, 3, 4, 5])
+# print(resultado)
+
+
+
+
+
+# def inverter(str):
+#     if len(str) <= 1:
+#         return str
+#     else:
+#         return str[-1] + inverter(str[:-1])
     
+# str = "pedro"
+# print(inverter(str))
+
+
+
